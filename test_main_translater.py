b4d17b4dc246f30d9af105bbe7a1ef8ecdb7eef6
SergTa <tabakar456@mail.ru>
2023-12-25 18:01:22 +0300

    Added translation_text

--- test_main_translater.py
+++ test_main_translater.py
@@ -15,7 +15,7 @@ def test_predict_1():   # Функция проверки ответа на по
                            json={"text": "I like machine learning!"})
     json_data = response.json()
     assert response.status_code == 200
-    assert json_data['label'] == 'Я люблю машинное обучение!'
+    assert json_data['translation_text'] == 'Я люблю машинное обучение!'
 
 
 def test_predict_2(): # Функция проверки ответа на пост-запрос другого варианта перевода
@@ -23,4 +23,4 @@ def test_predict_2(): # Функция проверки ответа на пос
                            json={"text": "We hate testing!"})
     json_data = response.json()
     assert response.status_code == 200
-    assert json_data['label'] == 'Мы ненавидим тесты!'
+    assert json_data['translation_text'] == 'Мы ненавидим тесты!'
