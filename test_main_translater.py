18e7cee64cb265e72d3436899ec46e3f5ca702f9
Tabakar Sergey <sergey.tabakar@nami.ru>
2024-02-27 16:17:49 +0300

    Edited for flake8

--- test_main_translater.py
+++ test_main_translater.py
@@ -1,28 +1,27 @@
-from fastapi.testclient import TestClient # Импорт тест - клиента
-from main_translater import app # Импорт объекта арр из файла main
+from fastapi.testclient import TestClient  # Импорт тест - клиента
+from main_translater import app  # Импорт объекта арр из файла main
 
-client = TestClient(app) # Создание клиента тестирования
+client = TestClient(app)  # Создание клиента тестирования
 
 
-def test_read_main(): # Функция проверки ответа на гет - запрос в корне
+def test_read_main():  # Функция проверки ответа на гет - запрос в корне
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
 
-def test_predict_1(): # Функция проверки ответа на пост - запрос варианта 
+def test_predict_1():  # Функция проверки ответа на пост - запрос варианта
     response = client.post(
-        "/predict/", 
-        json = {"text": "I like machine learning!"})
-    json_data = response.json()
+        "/predict/",
+        json={"text": "I like machine learning!"})
+    json_data=response.json()
     assert response.status_code == 200
     assert json_data['translation_text'] == 'Я люблю машинное обучение!'
 
 
-def test_predict_2(): # Функция проверки ответа на пост - запрос друг варианта
-    response = client.post("/predict/",
-        json = {"text": "We hate testing!"})
-    json_data = response.json()
+def test_predict_2():  # Функция проверки ответа на пост - запрос друг варианта
+    response=client.post("/predict/",
+    json={"text": "We hate testing!"})
+    json_data=response.json()
     assert response.status_code == 200
     assert json_data['translation_text'] == 'Мы ненавидим тесты!'
-    
