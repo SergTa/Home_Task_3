b6a7b3480ec1e0401a962ee4e4a54535f77a81ec
Tabakar Sergey <sergey.tabakar@nami.ru>
2024-02-27 15:32:40 +0300

    Edited for flake8

--- main_translater.py
+++ main_translater.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI   #Импорт fastapi 
-from transformers import pipeline   # импорт пайплайн для обращения к библиотеке преодученных моделей Hugg Face
+from transformers import pipeline  #импорт пайплайн обращения к Hugg Face
 from pydantic import BaseModel    # импорт ограничителя ввода данных
 
 class Item(BaseModel):   # ограничение ввода данных - только строка
@@ -7,15 +7,19 @@ class Item(BaseModel):   # ограничение ввода данных - то
 
 
 app = FastAPI()   # присвоение переменной класса FastAPI
-translator = pipeline(task ='translation', #  вызов задачи и  модели из библиотеки
-    model = 'Helsinki-NLP/opus-mt-en-ru')
+translator = pipeline(task = 'translation', #вызов задачи и модели из библ
+    model = 'Helsinki - NLP / opus - mt - en - ru')
 
 
-@app.get("/")  # Что делать при обращении в корень
+@app.get(" / ")  # Что делать при обращении в корень
+
+
 def root():
     return {"message": "Hello World"}
 
 
-@app.post("/predict/")  # Что делать при обращении в каталог predict
+@app.post(" / predict / ")  # Что делать при обращении в каталог predict
+
+
 def predict(item: Item):
     return translator (item.text )[0]
--- test_main_translater.py
+++ test_main_translater.py
@@ -1,26 +1,27 @@
-from fastapi.testclient import TestClient #Импорт тест-клиента
+from fastapi.testclient import TestClient #Импорт тест - клиента
 from main_translater import app           #Импорт объекта арр из файла main
 
 client = TestClient(app)  #Создание клиента тестирования
 
 
-def test_read_main():   # Функция проверки ответа на гет-запрос в корневой каталог
-    response = client.get("/")
+def test_read_main():# Функция проверки ответа на гет - запрос в корне
+    response = client.get(" / ")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
 
-def test_predict_1():   # Функция проверки ответа на пост-запрос варианта перевода
-    response = client.post("/predict/",
-                           json={"text": "I like machine learning!"})
+def test_predict_1():# Функция проверки ответа на пост - запрос варианта 
+    response = client.post(
+        " / predict / ", 
+        json = {"text": "I like machine learning!"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['translation_text'] == 'Я люблю машинное обучение!'
 
 
-def test_predict_2(): # Функция проверки ответа на пост-запрос другого варианта перевода
-    response = client.post("/predict/",
-                           json={"text": "We hate testing!"})
+def test_predict_2():# Функция проверки ответа на пост - запрос друг варианта
+    response = client.post(" / predict / ",
+        json = {"text": "We hate testing!"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['translation_text'] == 'Мы ненавидим тесты!'
