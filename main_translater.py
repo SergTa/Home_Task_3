3b27b325177ecb5b92fb8d4e25d88c9428fd9a4b
Tabakar Sergey <sergey.tabakar@nami.ru>
2024-02-27 14:56:12 +0300

    Edited for HT_2_1

--- main_translater.py
+++ main_translater.py
@@ -5,15 +5,17 @@ from pydantic import BaseModel    # импорт ограничителя вво
 class Item(BaseModel):   # ограничение ввода данных - только строка
     text: str
 
+
 app = FastAPI()   # присвоение переменной класса FastAPI
 translator = pipeline(task ='translation', #  вызов задачи и  модели из библиотеки
     model = 'Helsinki-NLP/opus-mt-en-ru')
+
 
 @app.get("/")  # Что делать при обращении в корень
 def root():
     return {"message": "Hello World"}
 
+
 @app.post("/predict/")  # Что делать при обращении в каталог predict
 def predict(item: Item):
     return translator (item.text )[0]
-
